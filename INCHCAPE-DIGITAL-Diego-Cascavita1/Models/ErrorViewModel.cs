aff7e98395d608a9f3a056e834a026927a41fa02
diego cascavita <sportscenter857@gmail.com>
2023-08-08 11:45:32 -0500

    Agregar archivos de proyecto.

---
+++ INCHCAPE-DIGITAL-Diego-Cascavita1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INCHCAPE_DIGITAL_Diego_Cascavita1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
